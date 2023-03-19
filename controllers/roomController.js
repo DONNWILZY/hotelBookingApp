beedd2d9e99d1e7cafa305289d724d7da67e4297
donnwilzy <ask4godswill@gmail.com>
2023-03-19 23:41:01 +0100

    room controllers aded

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -29,3 +29,65 @@ export const createRoom = async (req, res, next)=>{
     }
 }
 
+////// UPDATE HOTEL
+export const updateRoom = async (req, res, next) =>{
+    try{
+        const updatedRoom = await Room.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body},
+            { new: true }
+          );
+          res.json({
+            status: 200,
+            message: `Room with ID ${req.params.id} updated`,
+            data: updatedRoom,
+          });
+        //catch blog for update
+    }catch(err){
+        next(err);
+    }
+}
+
+/////DELETE HOTEL
+export const deleteRoom = async (req, res, next) =>{
+    try{
+        await Room.findByIdAndDelete(req.params.id);
+        res.json({
+            status: 200,
+            message: `Room with ID:  ${req.params.id} deleted succesfully`
+        })
+        //catch blog for delete
+    }catch(err){
+        next(err);
+    }
+}
+
+
+/////// GET HOTEL BY ID
+export const getRoom = async (req, res, next) =>{
+    try{
+        const Room = await Room.findById(req.params.id);
+        res.json({
+            status: 200,
+            message: `hotel with this ID(s) ${req.params.id} found`,
+            data: room
+        })
+
+    }catch(err){
+        next(err);
+    }
+}
+
+/////GET ALL Rooms
+export const getAllRooms = async (req, res, next) =>{
+    try{
+        const rooms = await Room.find();
+        res.json({
+            status: 200,
+            message: rooms
+        })
+
+    }catch(err){
+        next(err);
+    }
+}
